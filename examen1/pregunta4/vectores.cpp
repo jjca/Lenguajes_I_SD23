26809ed3fd5253e76a5e39ee612641cf3b21e20b
Jorge Correia <14-10254@usb.ve>
2023-10-13 20:13:22 -0400

    Updated pregunta 4 - Falta Unit testing de C++

--- examen1/pregunta4/vectores.cpp
+++ examen1/pregunta4/vectores.cpp
@@ -1,5 +1,6 @@
 #include "iostream"
 #include "cmath"
+#include "cstdlib"
 using namespace std;
 
 namespace espacioDeVectores {
@@ -87,52 +88,64 @@ namespace espacioDeVectores {
         return resultado;
     }
 
+    // Resta vector con float a derecha
+    Vector operator -(const Vector& vector1, const float& numero)
+    {
+        Vector resultado(0,0,0);
+        resultado.valor_x = vector1.valor_x-numero;
+        resultado.valor_y = vector1.valor_y-numero;
+        resultado.valor_z = vector1.valor_z-numero;
+        return resultado;
+    }
+
+    // Resta vector con float a izquierda
+    Vector operator -(const float& numero,const Vector& vector1)
+    {
+        Vector resultado(0,0,0);
+        resultado.valor_x = vector1.valor_x-numero;
+        resultado.valor_y = vector1.valor_y-numero;
+        resultado.valor_z = vector1.valor_z-numero;
+        return resultado;
+    }
+
     // Producto punto
-    float operator% (const Vector& vector1, const Vector& vector2) {
+    float operator%(const Vector& vector1, const Vector& vector2) {
         float punto = vector1.valor_x*vector2.valor_x + vector1.valor_y*vector2.valor_y + vector1.valor_z*vector2.valor_z;
         return punto;
     }
 
     // Modulo
-    float operator & (const Vector& vector) {
+    float operator& (const Vector& vector) {
         float modulo = sqrt(pow(vector.valor_x,2)+pow(vector.valor_y,2)+pow(vector.valor_z,2));
         return modulo;
     }
     
 
 };
-/*         private: 
-            int valor1, valor2;
-         */
-/*         public:
-            Vector (int a = 0, int b = 0)
-            {
-                valor1 = a;
-                valor2 = b;
-            } */
 
-/* 
-            void operator & () {
-                valor1+6;
-            }; */
-  /*   }
-}; */
 using namespace espacioDeVectores;
 int main(void) {
     std::cout << "hello world";
-    Vector a(-3.0,4.0,-3.0);
-    Vector b(-123.0,0.0,0.0);
-    Vector c(-3,0.5,5);
+    Vector a(rand()%10,rand()%10,rand()%10);
+    Vector b(rand()%10,rand()%10,rand()%10);
+    Vector c(rand()%10,rand()%10,rand()%10);
     Vector res(0,0,0);
+    float res2;
     a.print();
     b.print();
     c.print();
     res = a+b;
     cout << "a+b";
     res.print();
+
+    res = b+a;
+    cout << "b+a";
+    res.print();
+    
     res = a*b+c;
     cout << "a*b+c";
     res.print();
+    
     res = (b+b)*(c-a);
     cout << "(b+b)*(c-a)";
     res.print();
@@ -146,7 +159,34 @@ int main(void) {
     res.print();
     //res = a % (c*b);
     res = a * 3.0 + &b;
-    cout << "";
+    cout << "a*3.0+&b";
     res.print();
+    res = a * 3.0 + &b;
+    cout << "3.0*a+&b";
+    res.print();
+
+    res = (b+b);
+    cout << "b+b";
+    res.print();
+
+
+    res2 = c % a;
+    cout << "c%a" << res2 << endl;
+
+    res2 = a % c;
+    cout << "a%c" << res2 << endl;
+
+    res2 = a%(c*b);
+    cout << "a%(c*b)" << res2 << endl;
+
+    res = (b+b) * (c%a);
+    cout << "(b+b) * (c%a);";
+    res.print();
+
+    res = (c%a)*(b+b);
+    cout << " (c%a)*(b+b);";
+    res.print();
+
+    
     return 0;
 };
