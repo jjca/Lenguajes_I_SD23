c3f4fd1de80ceebcca1d1abcd60995b4bedbff6b
Jorge Correia <14-10254@usb.ve>
2023-11-16 18:25:03 -0400

    1bi casi lista?

--- examen2/pregunta1/pregunta1bi.c
+++ examen2/pregunta1/pregunta1bi.c
@@ -7,31 +7,59 @@ struct Church {
     char *valor;
 };
 
-Church sucesor (Church a) {
-    Church b = {NULL,malloc(2*sizeof(a.valor))};
+Church* sucesor(Church* a)
+{
+    printf("hola\n");
+    Church *b = NULL;
+    b = (Church*)malloc(sizeof(Church));
+    a->sucesor = b;
+    printf("UWU");
+    char *av = a->valor;
+    printf("%s \n",av);
+    char *tmp = (char*)malloc(strlen(av));
+    char *tmp2 = ")";
+    char *base = "Suc(";
+    strcat(tmp,"Suc(");
+    strncat(tmp,av,strlen(av));
+    strcat(tmp,")");
+    b->valor = (char*)malloc(strlen(tmp));
+    strncpy(b->valor,tmp,strlen(tmp));
+    free(tmp);
+    //strncat(tmp,tmp2,1);
+    //strncat(b->valor,tmp,sizeof(tmp));
+    printf("El valor del retornado es: %s\n",b->valor);
+    //Church *suca = a->sucesor;
+    //printf("el sucesor de %s es %s \n",a->valor,suca->valor);
+    return b;
+}
+
+/* Church sucesor (Church *a) {
+    Church b = {NULL,NULL};
+    //printf("el valor %s\n",sizeof(a.valor));
     a.sucesor = &b;
-    char tmp[20] = "Suc(";
+    char tmp[36] = "Suc(";
     char *tmp2 = ")";
     strncat(tmp,a.valor,sizeof(tmp));
     strncat(tmp,tmp2,1);
     strncat(b.valor,tmp,sizeof(tmp));
-    printf("El valor recibido es: %d\n",a.valor);
     printf("El valor del retornado es: %s\n",b.valor);
+    Church *suca = a.sucesor;
+    printf("el sucesor de %s es %s \n",a.valor,suca->valor);
     return b;
-}
+} */
 
-Church sucesorn (int n)
+Church* sucesorn (int n, Church* a)
 {
-    Church b = {NULL,malloc(2*n)};
+    Church *b = NULL;
+    b = (Church*)malloc(sizeof(Church));
+    b = sucesor(a);
+    printf("valor de b: %s\n",b->valor);
     for (int i = 0; i < n; i++)
-    {
-        char tmp[20] = "Suc(";
-        char *tmp2 = ")";
-        strncat(tmp,(char*)&n,sizeof(tmp));
-        strncat(tmp,tmp2,1);
-        strncat(b.valor,tmp,sizeof(tmp));
-    }
-    printf("Uwito %s",b.valor);
+        {
+            b = sucesor(b);
+        };
+    printf("Uwito %s\n",b->valor);
+    printf("el sucesor es %s\n",b->sucesor);
     return b;
 }
 /*
@@ -55,23 +83,36 @@ Church sumaChurch (Church a, Church b)
 }*/
 
 int main() {
-    
-    Church zero = {NULL,"zero"};
-    Church *x = NULL;
-    x = (Church*)malloc(sizeof(Church));
-    
-    Church y = sucesor(zero);
-    Church z = sucesor(y);
-
-    Church j = sucesor(y);
-
-    Church uno = sucesor(zero);
-    Church dos = sucesor(uno);
-    printf("El sucesor de cero es: %s\n",zero.sucesor);
-    printf("El sucesor de uno es: %s\n",uno.sucesor);
-    printf("El valor de uno es: %s\n",uno.valor);
-    printf("El valor de dos es: %s\n",dos.valor);
-    sucesorn(5);
+    Church *zero = NULL;
+    zero = (Church*)malloc(sizeof(Church));
+    zero->valor = "zero";
+    printf("El valor de cero es: %s\n",zero->valor);
+    char *a = zero->valor;
+    printf("%s \n",a);
+    char *tmp = (char*)malloc(strlen(a)+4);
+    char *tmp2 = ")";
+    char *base = "Suc(";
+    strncat(tmp,base,4);
+    strncat(tmp,a,strlen(a));
+    strncat(tmp,tmp2,1);
+    printf("jklfas %s\n",tmp);
+    /* char tmp[36] = "Suc(";
+    char *tmp2 = ")";
+    strncat(tmp,a.valor,sizeof(tmp));
+    strncat(tmp,tmp2,1);
+    strncat(b.valor,tmp,sizeof(tmp)); */
+    Church *uno = sucesor(zero);
+    printf("El sucesor de zero es: %p\n",&zero->sucesor);
+    printf("El valor del sucesor de cero es %s\n",uno->valor);
+    /*Church *dos = sucesor(uno); 
+    Church *tres = sucesor(dos);
+    Church *cuatro = sucesor(tres);
+    printf("El valor de cero es %s\n",zero->valor);
+    printf("El sucesor de cero es: %s y su valor es: %s \n",zero->sucesor,uno->valor);
+    printf("El sucesor de uno es: %s\n",uno->sucesor);
+    printf("El valor de uno es: %s\n",uno->valor);
+    printf("El valor de dos es: %s\n",dos->valor);*/
+    sucesorn(5,zero);
     //printf("%d \n",zero.zero);
     return 0;
 }
