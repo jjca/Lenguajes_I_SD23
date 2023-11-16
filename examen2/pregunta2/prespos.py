883e63def874a641922d016e208e657898c461f9
Jorge Correia <14-10254@usb.ve>
2023-11-16 13:36:07 -0400

    pregunta 2 completa.

--- examen2/pregunta2/prespos.py
+++ examen2/pregunta2/prespos.py
@@ -1,135 +1,84 @@
 import re
-
-class memoryNoAvailable(Exception):
-    "La capacidad es menor a la requerida"
-    pass
-
-class inputError(Exception):
-    "Error de entrada"
-    pass
-class programDoesNotExists(Exception):
-    "No existe el programa"
-    pass
-
-class programDoesExists(Exception):
-    "No existe el programa"
-    pass
-
 class commandIncomplete(Exception):
     "Comando incompleto"
     pass
-
 class commandWrongSyntax(Exception):
     "El comando está incorrecto"
     pass
 
 def evalPrefijo(string):
     stack = []
-    print(f"string oriignal {string}")
     for elem in string[::-1]:
         if re.match(r"[0-9]+",elem):
-            print(f"{elem} es un numero")
             stack.append(elem)
         else:
-            print(f"{elem} es un operador")
             num1 = stack.pop()
             num2 = stack.pop()
             operador = elem
             if operador == "/":
                 operador = "//"
             res = eval(num1+""+""+operador+""+""+num2)
-            print(f"olaaa {res}")
             stack.append(str(res))
-    print(stack)
+    print(stack[0])
 
 def evalPostfijo(string):
     stack = []
-    print(f"string oriignal {string}")
     for elem in string:
         if re.match(r"[0-9]+",elem):
-            print(f"{elem} es un numero")
             stack.append(elem)
         else:
-            print(f"{elem} es un operador")
             num1 = stack.pop()
             num2 = stack.pop()
             operador = elem
             if operador == "/":
                 operador = "//"
             res = eval(num2+""+""+operador+""+""+num1)
-            print(f"olaaa {res}")
             stack.append(str(res))
-    print(stack)
+    print(stack[0])
 
 def transPreToIn(string):
     stack = []
-    oldexpr = None
-    exprFinal = []
-    print(f"string oriignal {string}")
     dictPrec = {"+":"1","-":"1","*":"2","/":"2"}
     conmutativos = ["+","*"]
     for elem in string[::-1]:
         if re.match(r"[0-9]+",elem):
-            print(f"{elem} es un numero")
             stack.append((elem,None))
         else:
-            print(f"{elem} es un operador")
             expr1, operadorExpr1 = stack.pop()
-            print(f"expr1 {expr1,operadorExpr1}")
             expr2, operadorExpr2 = stack.pop()
-            print(f"expr2 {expr2,operadorExpr2}")
             operador = elem
             if operadorExpr1 is not None and dictPrec.get(operador) > dictPrec.get(operadorExpr1): 
-                print(f"ELIF 2 : La precedencia del operador {operador} es mayor a la de {operadorExpr1}")
                 expr1 = "("+expr1+")"
             if operadorExpr2 is not None and dictPrec.get(operador) > dictPrec.get(operadorExpr2): 
-                print(f"ELIF 3 : La precedencia del operador {operador} es mayor a la de {operadorExpr2}")
                 expr2 = "("+expr2+")"
             if operadorExpr2 is not None and dictPrec.get(operador) == dictPrec.get(operadorExpr2) and operador not in conmutativos: 
-                print(f"ELIF 4 : La precedencia del operador {operador} es mayor a la de {operadorExpr2}")
                 expr2 = "("+expr2+")"
             newexpr = expr1+operador+expr2
             stack.append((newexpr,operador))
-            print(newexpr)
-                
-    exprFinal.append(newexpr)
-    print(exprFinal[-1])
+    print(newexpr)
 
 def transPostToIn(string):
     stack = []
-    exprFinal = []
-    print(f"string oriignal {string}")
     dictPrec = {"+":"1","-":"1","*":"2","/":"2"}
     conmutativos = ["+","*"]
     for elem in string:
         if re.match(r"[0-9]+",elem):
-            print(f"{elem} es un numero")
             stack.append((elem,None))
         else:
-            print(f"{elem} es un operador")
-            expr1, operadorExpr1 = stack.pop()
-            print(f"expr1 {expr1,operadorExpr1}")
             expr2, operadorExpr2 = stack.pop()
-            print(f"expr2 {expr2,operadorExpr2}")
+            expr1, operadorExpr1 = stack.pop()
             operador = elem
-            if operadorExpr2 is not None and dictPrec.get(operador) < dictPrec.get(operadorExpr2): 
-                print(f"ELIF 2 : La precedencia del operador {operador} es mayor a la de {operadorExpr1}")
-                expr1 = "("+expr2+")"
-            if operadorExpr1 is not None and dictPrec.get(operador) < dictPrec.get(operadorExpr1): 
-                print(f"ELIF 3 : La precedencia del operador {operador} es mayor a la de {operadorExpr2}")
-                expr2 = "("+expr1+")"
-            if operadorExpr1 is not None and dictPrec.get(operador) == dictPrec.get(operadorExpr1) and operador not in conmutativos: 
-                print(f"ELIF 4 : La precedencia del operador {operador} es mayor a la de {operadorExpr2}")
-                expr2 = "("+expr1+")"
+            if operadorExpr1 is not None and dictPrec.get(operador) > dictPrec.get(operadorExpr1): 
+                expr1 = "("+expr1+")"
+            if operadorExpr2 is not None and dictPrec.get(operador) > dictPrec.get(operadorExpr2): 
+                expr2 = "("+expr2+")"
+            if operadorExpr2 is not None and dictPrec.get(operador) == dictPrec.get(operadorExpr2) and operador not in conmutativos: 
+                expr2 = "("+expr2+")"
             newexpr = expr1+operador+expr2
             stack.append((newexpr,operador))
-            print(newexpr)
-                
-    exprFinal.append(newexpr)
-    print(exprFinal[-1])
+    print(newexpr)
 
 def main():
-    print("Hola")
     activo = True
     while activo:
         entrada = list(input("Introduzca una opción válida. Para más información escriba HELP:  ").split())
@@ -143,35 +92,15 @@ def main():
                         orden = entrada[1].lower()
                         try: 
                             if 'pre' == orden:
-                                print("Prefijo")
                                 evalPrefijo(entrada[2:])
                             elif 'post' == orden:
-                                print("postfijo")
                                 evalPostfijo(entrada[2:])
                             else:
                                 raise ValueError
-                        except memoryNoAvailable:
-                            print("uwu")
+                        except commandWrongSyntax:
                             continue
                     else:
                         raise commandIncomplete
-                    """elif len(entrada[2].lower()) == 0 or len(entrada) < 3:
-                            print("Nombre del programa vacio")
-                            raise inputError
-                        if (not memory.searchProgram(entrada[2].lower())):
-                            memory.addProgram(entrada[2].lower(),entrada[2],quantity)
-                            print (f"Memoria reservada para el programa {entrada[2]}")
-                        else:
-                            raise programDoesExists
-                    except memoryNoAvailable:
-                        print("Memoria no disponible para reservar")
-                        continue
-                    except inputError:
-                        print("Por favor introduzca los datos correctamente")
-                        continue
-                    except programDoesExists:
-                        print("El programa ya existe No puede reservarse memoria")
-                        continue"""
                 except commandIncomplete:
                     print("Faltan argumentos en el comando EVAL")
                     continue
@@ -191,8 +120,7 @@ def main():
                                 transPostToIn(entrada[2:])
                             else:
                                 raise ValueError
-                        except memoryNoAvailable:
-                            print("uwu")
+                        except commandWrongSyntax:
                             continue
                     else:
                         raise commandIncomplete
