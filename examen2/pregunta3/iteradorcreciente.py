1b78de4378a375f4e9cfa5d4acf2d8e08a11b6db
Jorge Correia <14-10254@usb.ve>
2023-11-18 00:05:10 -0400

    updated

--- examen2/pregunta3/iteradorcreciente.py
+++ examen2/pregunta3/iteradorcreciente.py
@@ -1,24 +1,27 @@
-from itertools import combinations
-
 def obtenerCrecientes(lista):
     crecientes = []
     for i in range(0,len(lista)):
         if(all(lista[i][j] <= lista[i][j + 1] for j in range(len(lista[i])-1))):
             crecientes.append(lista[i])
-            print(f"{lista[i]}")
-
+            #print(f"{lista[i]}")
+    return crecientes
 
 def obtenerParticiones(lista):
-    particiones = list()
-    for i in range(len(lista)+1):
-        particiones += list(combinations(lista,i))
-    return particiones    
+    if lista == []:
+        yield []
+    else:
+        for x in obtenerParticiones(lista[1:]):
+            yield x
+            yield [lista[0],*x]
 
 def main():
     print("Se tiene la lista:")
     lista = [1,4,3,2,5]
+    particiones= []
     print(lista)
-    particiones = obtenerParticiones(lista)
+    for x in obtenerParticiones(lista):
+        particiones.append(x)
+    #print(particiones)
     crecientes = obtenerCrecientes(particiones)
     print(crecientes)
 
